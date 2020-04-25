1e68b543c7a6b6ef1f953588cc562c848215cab0
Ciceronianus <Ciceronianus@outlook.com>
2020-04-25 14:14:34 +0200

    Smallk update

--- background_scripts/background_hypothesify.js
+++ background_scripts/background_hypothesify.js
@@ -15,14 +15,7 @@ async function getAnnotations(tabs) {
   });
 }
 
-// getAnnotations();
 
-async function getCurrentUrl(){
-  var tabs = await browser.tabs.query({currentWindow: true, active: true});
-  console.log("hello");
-  getAnnotations(tabs);
-
-}
 
 function writeBadge(){
 
@@ -31,6 +24,20 @@ function writeBadge(){
   });
 }
 
+function handleMessage(request, sender, sendResponse) {
+  console.log("Message from the content script: " +
+    request.greeting);
+  sendResponse({response: "Response from background script"});
+}
 
+// background-script.js
+
+function handleMessage(request, sender, sendResponse) {
+  console.log("Message from the content script: " +
+    request.greeting);
+  sendResponse({response: "Response from background script"});
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
 
 
--- content_scripts/content_hypothesify.js
+++ content_scripts/content_hypothesify.js
@@ -1,12 +1,28 @@
 // content.script
 
-async function getCurrentUrl(){
-  var tabs = await browser.tabs.query({currentWindow: true, active: true});
-  console.log("hello");
-  
-  browser.runtime.sendMessage({tabsToSend: tabs});
+// content-script.js
+
+function handleResponse(message) {
+  console.log(`Message from the background script:  ${message.response}`);
 }
 
-console.log("main script");
+function handleError(error) {
+  console.log(`Error: ${error}`);
+}
 
-getCurrentUrl();
+function notifyBackgroundPage(e) {
+  var sending = browser.runtime.sendMessage({
+    greeting: "Greeting from the content script"
+  });
+  sending.then(handleResponse, handleError);  
+}
+
+/* the loading */
+let url = window.location.href;
+console.log(url);
+
+window.addEventListener('load', (event) => {
+  console.log('page is fully loaded');
+});
+
+window.addEventListener("click", notifyBackgroundPage);
